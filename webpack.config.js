701354955566da28a9cde919ceee962c1a59c23d
Cayce Collins <cayce.collins@gmail.com>
2017-10-19 03:09:12 -0700

    refactored to use es6 class component

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,38 @@
-var webpack = require("webpack")
+var path = require('path')
+
+var webpack = require('webpack')
 
 module.exports = {
   entry: {
-    "react-chartkick": "./index.js",
-    "react-chartkick.min": "./index.js"
+    'react-chartkick': './index.js',
+    'react-chartkick.min': './index.js',
   },
   output: {
-    path: "./dist",
-    filename: "[name].js",
-    libraryTarget: "umd"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: false,
+          presets: ['react', ['env', { modules: false }]],
+        },
+      },
+    ],
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       include: /\.min\.js$/,
-      minimize: true
-    })
+      minimize: true,
+    }),
   ],
   externals: {
-    "react": "React",
-    "chartkick": "Chartkick"
-  }
+    react: 'React',
+    chartkick: 'Chartkick',
+  },
 }
